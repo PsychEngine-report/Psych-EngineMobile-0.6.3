1a6cbdc9aeb44da3d14fd62e81cd801c6d9aa7d8
Ethantobot <156458071+Ethantobot11@users.noreply.github.com>
2025-09-25 11:24:40 +0200

    Update Main.hx

--- source/Main.hx
+++ source/Main.hx
@@ -13,7 +13,6 @@ import openfl.display.StageScaleMode;
 import lime.system.System as LimeSystem;
 #if mobile
 import mobile.CopyState;
-import mobile.MobileScaleMode
 #end
 
 #if windows
@@ -37,7 +36,7 @@ class Main extends Sprite
 {
 	var gameWidth:Int = 1280; // Width of the game in pixels (might be less / more in actual pixels depending on your zoom).
 	var gameHeight:Int = 720; // Height of the game in pixels (might be less / more in actual pixels depending on your zoom).
-	var zoom:Float = -1; // game state bounds
+	var zoom:Float = -1; // If -1, zoom is automatically calculated to fit the window dimensions.
 	var initialState:Class<FlxState> = TitleState; // The FlxState the game starts with.
 	var framerate:Int = 60; // How many frames per second the game should run at.
 	var skipSplash:Bool = true; // Whether to skip the flixel splash screen that appears in release mode.
