41aee520783aeef9e9b8daa4bb86c92966bc8578
Ethantobot <ethantobot7@gmail.com>
2026-07-16 22:25:39 +0200

    Update WeekEditorState.hx

--- source/editors/WeekEditorState.hx
+++ source/editors/WeekEditorState.hx
@@ -335,7 +335,7 @@ class WeekEditorState extends MusicBeatState
 		var isMissing:Bool = true;
 		if(assetName != null && assetName.length > 0) {
 			if( #if MODS_ALLOWED FileSystem.exists(Paths.modsImages('menubackgrounds/menu_' + assetName)) || #end
-			Assets.exists(Paths.getPath('images/menubackgrounds/menu_' + assetName + '.png', IMAGE), IMAGE || Assets.exists(Paths.getPath('images/menubackgrounds/menu_' + assetName + '.astc', IMAGE), IMAGE))) {
+			Assets.exists(Paths.getPath('images/menubackgrounds/menu_' + assetName + '.png', IMAGE), IMAGE || Assets.exists(Paths.getPath('images/menubackgrounds/menu_' + assetName + '.astc', BINARY), BINARY))) {
 				bgSprite.loadGraphic(Paths.image('menubackgrounds/menu_' + assetName));
 				isMissing = false;
 			}
@@ -354,7 +354,7 @@ class WeekEditorState extends MusicBeatState
 		var isMissing:Bool = true;
 		if(assetName != null && assetName.length > 0) {
 			if( #if MODS_ALLOWED FileSystem.exists(Paths.modsImages('storymenu/' + assetName)) || #end
-			Assets.exists(Paths.getPath('images/storymenu/' + assetName + '.png', IMAGE), IMAGE || Assets.exists(Paths.getPath('images/storymenu/' + assetName + '.astc', IMAGE), IMAGE))) {
+			Assets.exists(Paths.getPath('images/storymenu/' + assetName + '.png', IMAGE), IMAGE || Assets.exists(Paths.getPath('images/storymenu/' + assetName + '.astc', BINARY), BINARY))) {
 				weekThing.loadGraphic(Paths.image('storymenu/' + assetName));
 				isMissing = false;
 			}
