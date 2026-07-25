b4520750f26bf25e6a6841ca4fc5fac68c9621e6
Ethantobot <ethantobot7@gmail.com>
2026-07-25 19:15:30 +0200

    ok

--- source/import.hx
+++ source/import.hx
@@ -1,3 +1,4 @@
+#if !macro
 import Paths;
 import mobile.StorageUtil;
 #if android
@@ -8,4 +9,5 @@ import android.Settings as AndroidSettings;
 import android.Tools as AndroidTools;
 import android.os.Build.VERSION as AndroidVersion;
 import android.os.Build.VERSION_CODES as AndroidVersionCode;
+#end
 #end
