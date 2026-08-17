81a21c449d60b68719c1c82ce45c68a5c250649f
Ethantobot <ethantobot7@gmail.com>
2026-08-17 22:46:19 +0200

    Update HealthIcon.hx

--- source/HealthIcon.hx
+++ source/HealthIcon.hx
@@ -38,9 +38,8 @@ class HealthIcon extends FlxSprite
 	public function changeIcon(char:String) {
 		if(this.char != char) {
 			var name:String = 'icons/' + char;
-			if(!Paths.fileExists('images/' + name + '.png', IMAGE) || !Paths.fileExists('images/' + name + '.astc', BINARY)) name = 'icons/icon-' + char; //Older versions of psych engine's support
-			if(!Paths.fileExists('images/' + name + '.png', IMAGE) || !Paths.fileExists('images/' + name + '.astc', BINARY)) name = 'icons/icon-face'; //Prevents crash from missing icon
-			var file:Dynamic = Paths.image(name);
+			if(!Paths.fileExists('images/' + name + '.png', IMAGE) && !Paths.fileExists('images/' + name + '.astc', BINARY) && !Paths.fileExists('images/' + name + '.dds', BINARY)) name = 'icons/icon-' + char; //Older versions of psych engine's support
+		    if(!Paths.fileExists('images/' + name + '.png', IMAGE) && !Paths.fileExists('images/' + name + '.astc', BINARY) && !Paths.fileExists('images/' + name + '.dds', BINARY)) name = 'icons/icon-face'; //Prevents crash from missing icon
 
 			loadGraphic(file); //Load stupidly first for getting the file size
 			loadGraphic(file, true, Math.floor(width / 2), Math.floor(height)); //Then load it fr
